47448f12cde8052aaefb470128f141e5118a98d4
Vlads <98471773+Golotniks94@users.noreply.github.com>
2026-04-25 20:24:11 +0300

    First Final Push
    HW-6  first final push

--- js/index.js
+++ js/index.js
@@ -16,3 +16,21 @@ backdrop.addEventListener('click', e => {
     backdrop.classList.remove('is-open');
   }
 });
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    backdrop.classList.remove('is-open');
+  }
+});
+
+const menuOpenBtn = document.querySelector('.menu-open-btn');
+const menuCloseBtn = document.querySelector('.menu-close-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuOpenBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('is-open');
+});
+
+menuCloseBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+});
